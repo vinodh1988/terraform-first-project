179d7e61b00897d41ddc6264d489185ff1082bb1
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-16 14:53:12 +0530

    latest

--- resource.tf
+++ resource.tf
@@ -2,3 +2,22 @@ resource "azurerm_resource_group" "terra" {
   name     = "terra"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "storeterra" {
+    name     = "example-resources"
+    location = "East US"
+}
+
+resource "azurerm_storage_account" "saterra" {
+    name                     = "terrastorage238"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
+}
