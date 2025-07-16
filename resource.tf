e5e31f9fd15997334bde29aa3805fbbdcc1c7a91
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-16 15:31:33 +0530

    latest

--- resource.tf
+++ resource.tf
@@ -21,3 +21,24 @@ resource "azurerm_storage_container" "sacontainer" {
     storage_account_name  = azurerm_storage_account.saterra.name
     container_access_type = "private"
 }
+
+resource "azurerm_virtual_network" "terra_vnet" {
+    name                = "terra-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.terra.location
+    resource_group_name = azurerm_resource_group.terra.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+    name                 = "subnet1"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+    name                 = "subnet2"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
+}
